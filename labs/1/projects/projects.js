e939b7117df8b6179654654b9c045a4a3f3b9fb0
Shriya Pattapu <spattapu@ucsd.edu>
2025-04-25 22:27:56 -0700

    done lab 4

--- labs/1/projects/projects.js
+++ labs/1/projects/projects.js
@@ -2,7 +2,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 
 const projectsContainer = document.querySelector('.projects');
+const titleElement = document.querySelector('.projects-title');
 
 const projects = await fetchJSON('../lib/projects.json');
 
 renderProjects(projects, projectsContainer, 'h2');
+titleElement.textContent = `${projects.length} Projects`;
