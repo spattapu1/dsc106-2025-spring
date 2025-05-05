c474a1aad74cc14140a8415b7acda074e63d5195
Shriya Pattapu <spattapu@ucsd.edu>
2025-05-05 09:39:48 -0700

    Update global.js

--- labs/1/global.js
+++ labs/1/global.js
@@ -36,10 +36,10 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'resume/', title: 'Resume' },
-  { url: 'contact/', title: 'Contact' },
+  { url: './', title: 'Home' },
+  { url: './projects/', title: 'Projects' },
+  { url: './resume/', title: 'Resume' },
+  { url: './contact/', title: 'Contact' },
   { url: 'https://github.com/spattapu1', title: 'GitHub' },
 ];
 
